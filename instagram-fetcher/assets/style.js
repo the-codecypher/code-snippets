f18df6e1a18e25d17d4c78f92bdddc129be926b6
gurjotsaini <gurjotsaini1992@gmail.com>
2020-07-22 08:29:14 +0530

    CRM pt3

--- instagram-fetcher/assets/style.js
+++ instagram-fetcher/assets/style.js
@@ -1,27 +1,9 @@
-// const followers = document.getElementById('followers');
-
-// async function getFollowers(profileId) {
-//     const url = `https://instagram.com/${profileId}/?__a=1`;
-//     const response = await fetch(url);
-//     const data = await response.json();
-
-//     return data.graphql.edge_followed_by.count;
-// }
-
-// async function setFollowers(profileId) {
-//     followers.textContent = await getFollowers(profileId);
-// }
-
-// setInterval(() => setFollowers('codecypher.in'), 1000);
-
-
-
 const followers = document.getElementById('followers');
 
 async function getFollowers(profileId) {
-    const url      = `https://instagram.com/${profileId}/?__a=1`;
+    const url = `https://instagram.com/${profileId}/?__a=1`;
     const response = await fetch(url);
-    const data     = await response.json();
+    const data = await response.json();
 
     return data.graphql.user.edge_followed_by.count;
 }
